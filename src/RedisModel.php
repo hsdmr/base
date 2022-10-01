44602e2c39fd61b8c07818f1bbb56dde2b297287
hsdmr <hsdmrsoft@gmail.com>
2022-10-02 00:30:23 +0300

    added header function

--- src/RedisModel.php
+++ src/RedisModel.php
@@ -4,4 +4,16 @@ namespace Hasdemir\Base;
 
 class RedisModel
 {
+  protected string $key = '';
+
+  public function __construct()
+  {
+    Codes::currentJob($this->key . '-redis');
+    $this->redis = System::getRedis();
+  }
+
+  public function __destruct()
+  {
+    Codes::endJob();
+  }
 }
