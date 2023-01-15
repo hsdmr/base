4718a5e701ab999511c63fd07727e2b19314c9eb
Murat Hasdemir <hsdmrsoft@gmail.com>
2023-01-16 02:33:23 +0300

    improved controller

--- src/Controller.php
+++ src/Controller.php
@@ -10,10 +10,21 @@ class Controller
   protected array $header = [];
   protected ?array $body = null;
   protected ?string $link = null;
+  protected array $request_log = [];
 
-  public function __construct()
+  public function __construct($request, $args)
   {
     $GLOBALS[Codes::IS_ROUTE_CALLED] = true;
+    $this->request_log = [
+      'ip' => $request->ip,
+      'port' => $request->port,
+      'method' => $request->method,
+      'path' => $request->path,
+      'agent' => $request->agent,
+      'headers' => logMask($request->headers),
+      'body' => logMask($request->body),
+      'get' => $request->params,
+    ];
   }
 
   public static function routes(): array
@@ -23,6 +34,15 @@ class Controller
 
   public function response($http_code)
   {
+    Log::setContext([
+      'type' => 'API',
+      'status' => 'success',
+      'request' => $this->request_log,
+      'response' => [
+        'headers' => logMask($this->header),
+        'body' => logMask($this->body)
+      ]
+    ]);
     Response::emit($http_code, $this->header, $this->body ?? '');
   }
 }
